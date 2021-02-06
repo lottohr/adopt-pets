e5f4837e5eaf3880bd6906eaf7c5a7778b7991b1
lottohr <luci.jurin@gmail.com>
2021-02-06 02:56:27 +0100

    change single to double qoutes & remove white space in sort and filter scripts

--- src/js/helpers/sort.js
+++ src/js/helpers/sort.js
@@ -1,17 +1,15 @@
-function _sort(data, state){
-
-    if(state.sortOrder === 'asc' && state.sortBy ==='age'){
-        return data.sort((a, b) => a.age - b.age)
+function _sort(data, state) {
+    if (state.sortOrder === "asc" && state.sortBy === "age") {
+        return data.sort((a, b) => a.age - b.age);
     }
-    else if(state.sortOrder === 'desc' && state.sortBy ==='age'){
-        return data.sort((a, b) => b.age - a.age)
+    else if (state.sortOrder === "desc" && state.sortBy === "age") {
+        return data.sort((a, b) => b.age - a.age);
     }
-    else if(state.sortOrder === 'asc' && state.sortBy ==='name'){
-        return data.sort((a, b) => a.name.localeCompare(b.name))
+    else if (state.sortOrder === "asc" && state.sortBy === "name") {
+        return data.sort((a, b) => a.name.localeCompare(b.name));
     }
-    else if(state.sortOrder === 'desc' && state.sortBy ==='name'){
-        return data.sort((a, b) => b.name.localeCompare(a.name))
+    else if (state.sortOrder === "desc" && state.sortBy === "name") {
+        return data.sort((a, b) => b.name.localeCompare(a.name));
     }
 }
-
 export default _sort
