df34fee6d6a1653d8a0eb2f803d8f42fa855d1a5
lottohr <luci.jurin@gmail.com>
2021-02-06 02:56:27 +0100

    change single to double qoutes & remove white space in sort and filter scripts

--- src/js/helpers/filters.js
+++ src/js/helpers/filters.js
@@ -1,18 +1,13 @@
-function _filter(data, state){
-    
-    if(state.filterColor !== 'null'){
-        data = data.filter(el => el.color == state.filterColor.toLowerCase());    
+function _filter(data, state) {
+    if (state.filterColor !== "null") {
+        data = data.filter(el => el.color == state.filterColor.toLowerCase());
     }
-
-    if(state.filterAge1!== 'null'){
-        data = data.filter(el => el.age <= state.filterAge1);   
+    if (state.filterAge1 !== "null") {
+        data = data.filter(el => el.age <= state.filterAge1);
     }
-
-    if(state.filterAge2!== 'null'){
-        data = data.filter(el => el.age <= state.filterAge2);   
+    if (state.filterAge2 !== "null") {
+        data = data.filter(el => el.age <= state.filterAge2);
     }
-
-    return data
+    return data;
 }
-
 export default _filter
