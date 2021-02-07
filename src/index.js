3901aeee19c0121f3d96f51e6ff8046036318f7e
lottohr <luci.jurin@gmail.com>
2021-02-07 01:46:21 +0100

    format list.js & index.js

--- src/index.js
+++ src/index.js
@@ -1,70 +1,63 @@
-import listComponent from './js/components/list.js'
-import carouselComponent from './js/components/carousel.js'
+import listComponent from "./js/components/list.js";
+import carouselComponent from "./js/components/carousel.js";
 
 const carousel = async () => {
-    const response = await fetch('./data.json')
-    const data = await response.json()
+    const response = await fetch("./data.json");
+    const data = await response.json();
 
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     } else {
-
-        data.forEach((obj,id)=>{
+        data.forEach((obj, id) => {
             Object.assign(obj, {
-                id: id, 
+                id: id,
+                img: `img/${obj.name.toLowerCase()}.jpg`
+            })
+        })
+        let jsonData = data.sort((a, b) => a.age - b.age).slice(0, 4);
+        carouselComponent(jsonData);
+    }
+}
+
+const list = async () => {
+    const response = await fetch("./data.json");
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+        data.forEach((obj, id) => {
+            Object.assign(obj, {
+                id: id,
                 img: `img/${obj.name.toLowerCase()}.jpg`
             })
         })
 
-        let jsonData = data.sort((a, b) => a.age - b.age).slice(0, 4)
-        carouselComponent(jsonData)
-    }
-}
-
- const list = async () => {
-    const response = await fetch('./data.json')
-    const data = await response.json()
-
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    } else {
-
-        data.forEach((obj,id)=>{
-            Object.assign(obj, {
-                id: id, 
-                img: `img/${obj.name.toLowerCase()}.jpg`
-            }) 
-        })
-    
-        let jsonData = data.sort((a, b) => a.age - b.age)
-
-        let state ={
+        let jsonData = data.sort((a, b) => a.age - b.age);
+        let state = {
             currentItems: 20,
-            totalItems:data.length,
-            sortBy:'age',
-            sortOrder:'asc',
-            filterAge1:'null',
-            filterAge2:'null',
-            filterColor:'null',
-        }
-    
-        //List controller
-        listComponent(state, jsonData)
+            totalItems: data.length,
+            sortBy: "age",
+            sortOrder: "asc",
+            filterAge1: "null",
+            filterAge2: "null",
+            filterColor: "null",
+        };
 
+        //List controller
+        listComponent(state, jsonData);
     }
 }
 
-window.addEventListener('load', () => {
-
+window.addEventListener("load", () => {
     // Init components
-    carousel() 
-    list()
+    carousel();
+    list();
 
     // Remove modal on click outside
-    let modal= document.querySelector('.modal_overlay'); 
-    modal.addEventListener("click",function(e){    
-        document.getElementById('adoptModal').style.display="none"
-        document.body.classList.remove("modal-opened") 
-    }); 
-
+    let modal = document.querySelector(".modal_overlay");
+    modal.addEventListener("click", function (e) {
+        document.getElementById("adoptModal").style.display = "none";
+        document.body.classList.remove("modal-opened");
+    });
 });
