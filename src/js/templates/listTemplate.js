588016d2fc17a89e49964567ec2c7be1380dc2a0
lottohr <luci.jurin@gmail.com>
2021-02-07 01:12:49 +0100

    add search box message

--- src/js/templates/listTemplate.js
+++ src/js/templates/listTemplate.js
@@ -1,18 +1,21 @@
 export default function listTemplate(data){
-    let output = ''
-    data.forEach((item)=>{
-        output += `
-            <div id="${item.id}" class="card">
-            <img src="${item.img}" >
-                <div class="card__info">
-                    <h2> ${item.name}</h2>
-                    <p>Age: ${item.age}</p>
-                    <p>Color: ${item.color}</p>
+    let output = "";
+    if(data.length > 0){
+        data.forEach((item)=>{
+            output += `
+                <div id="${item.id}" class="card">
+                <img src="${item.img}" >
+                    <div class="card__info">
+                        <h2> ${item.name}</h2>
+                        <p>Age: ${item.age}</p>
+                        <p>Color: ${item.color}</p>
+                    </div>
+                    <button class="btn btn__adopt">Adopt me</button>
                 </div>
-                <button class="btn btn__adopt">Adopt me</button>
-            </div>
-        `;
-    });
-    
-    document.getElementById('listData').innerHTML = output
+            `;
+        });
+    }else{
+        output += "<h2>Sorry no results found</h2>";
+    }
+    document.getElementById("listData").innerHTML = output;
 }
