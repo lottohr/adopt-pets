2eed9a33cf74baa06472d921e44815859cfb037d
lottohr <luci.jurin@gmail.com>
2021-02-08 20:03:20 +0100

    add modal img & custom close btn

--- src/js/templates/modalTemplate.js
+++ src/js/templates/modalTemplate.js
@@ -1,10 +1,14 @@
 export default function modalTemplate(item){
     let output  = `
         <div id="modal_content" >
-        <button id="btnReject" class="btn btn_close" ><i class="fas fa-times-circle"></i></button>
+            <button id="btnReject" class="btn btn_close" >
+                <div class="bar1"></div>
+                <div class="bar2"></div> 
+            </button>
+            <img class="modal_img" src="${item[0].img}" >
             <h2 class="modal_title">Are you sure, you wanna adopt ${item[0].name}?</h2>
             <div id="itemID_${item[0].id}" >
-                <h2>Name: ${item[0].name}</h2>
+                <p>Name: ${item[0].name}</p>
                 <p>Color: ${item[0].color}</p>
                 <p>Age: ${item[0].age}</p>
             </div>
